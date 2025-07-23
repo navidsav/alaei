45d2ae8ec0999a2f69ae5161bbf5bebed35e219d
navidsavarib <navidsavarib@gmail.com>
2025-07-23 17:26:25 +0330

    get brands

--- mobile_api/routes/constant.js
+++ mobile_api/routes/constant.js
@@ -2,7 +2,6 @@ const express = require("express");
 const ColorModel = require("../models/ColorModel");
 const response_handler = require("./response_handler");
 const logger = require("../../common/logger");
-const { querymen, Schema } = require('querymen');
 
 
 const router = express.Router();
