bc6547e2b24508aad7201eebe6cb36c352779b72
navidsavarib <navidsavarib@gmail.com>
2025-07-28 00:02:24 +0330

    Ad Verify

--- mobile_api/routes/ad.js
+++ mobile_api/routes/ad.js
@@ -86,6 +86,92 @@ router.get("/add", authMiddleware, upload.array('images', 10), async (req, res) 
 
 
 
+const delivery_status = require("../../common/car/delivery_status");
+const body_status = require("../../common/car/body_status");
+const colors = require("../../common/car/colors");
+const colors_interior = require("../../common/car/colors_interior");
+const installment_number = require("../../common/car/installment_number");
+const installment_month = require("../../common/car/installment_month");
+const installment_delivery = require("../../common/car/installment_delivery");
+const payment_type = require("../../common/car/payment_type");
+router.get("/verify", authMiddleware, upload.array('images', 10), async (req, res) => {
+
+
+
+  try {
+
+    let {
+      trim_id,
+      production_year,
+      delivery_status_type,
+      delivery_status_value,
+      body_color_value,
+      interior_color_value,
+      body_status_value,
+      payment_type_value,
+      payment_total_price,
+      installment_number_value,
+      installment_delivery_days_value,
+      installment_month_value,
+
+    } = req.body;
+
+    let trim = await db.aggregate("carbrands", [
+      [
+        {
+          $unwind: {
+            path: "$CarModels"
+          }
+        },
+
+        {
+          $unwind: {
+            path: "$CarModels.CarModelDetails"
+          }
+        },
+        {
+          $match: {
+            "CarModels.CarModelDetails._id": new mongodb.ObjectId(trim_id)
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            BrandTitle: 1,
+            CarModelTitle: "$CarModels.CarModelTitle",
+            CarModelDetail: "$CarModels.CarModelDetails.CarModelDetailTitle"
+          }
+        }
+      ]
+    ])
+
+
+
+    // Respond with the car details
+    return response_handler.okResponse(res, "here you are", { 
+      trim:trim,
+      production_year:production_year,
+      delivery_status_type:delivery_status.delivery_type.find(o => o.value == delivery_status_type),
+      delivery_havele_type:delivery_status.havale_type.find(o => o.value == delivery_status_value),
+      body_color: colors.find(o => o.value == body_color_value),
+      interior_color: colors_interior.find(o => o.value == interior_color_value),
+      body_status : body_status.find(o => o.value == body_status_value),
+      payment_type: payment_type.find(o => o.value == payment_type_value),
+      payment_total_price:payment_total_price,
+      installment_number: installment_number.find(o => o.value == installment_number_value),
+      installment_month: installment_month.find(o => o.value == installment_month_value),
+      installment_delivery: installment_delivery.find(o => o.value == installment_delivery_days_value),
+      
+
+
+     })
+  } catch (error) {
+    logger.error({ event: "HTTP GET COLORS ERROR ", error: error?.message })
+    response_handler.errorResponse(res, "Server error", error)
+  }
+});
+
+
 // ############################################
 // ############################################
 // ############################################
