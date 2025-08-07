a71dc6cdc31c75db4442a63586225cd1faa53696
navidsavarib <navidsavarib@gmail.com>
2025-08-07 14:19:15 +0330

    Changes

--- common/code_generator.js
+++ common/code_generator.js
@@ -2,7 +2,7 @@
 
 module.exports = ({ agencyCode, year, cityName, personIndex }) => {
   const now = new Date();
-  const year = now.getFullYear().toString().slice(-2); // "25"
+  // const year = now.getFullYear().toString().slice(-2); // "25"
   const month = String(now.getMonth() + 1).padStart(2, '0'); // "08"
 
   const cityCode = cityName.slice(0, 3).toUpperCase(); // "Tehran" -> "TEH"
