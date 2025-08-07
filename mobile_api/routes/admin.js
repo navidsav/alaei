9ee63e308467607732553bc7d6ba7c920c97f6f4
navidsavarib <navidsavarib@gmail.com>
2025-08-07 16:31:27 +0330

    Changes

--- mobile_api/routes/admin.js
+++ mobile_api/routes/admin.js
@@ -91,109 +91,13 @@ router.post("/generateReferralCode", async (req, res) => {
 // ############################################
 // ############################################
 // ############################################
-router.get("/getall", async (req, res) => {
+router.get("/getCodes", async (req, res) => {
 
   try {
 
-    const carsList = await db.aggregate("users", [
-      {
-        $match: {
-          "_id": new mongodb.ObjectId(req.user.id)
-        },
-      },
-      {
-        $lookup: {
-          from: "carbrands",
-          localField: "registeredCars.carInfo.carModelId",
-          foreignField: "CarModels.CarModelDetails._id",
-          as: "carBrands"
-        },
-      },
-      {
-        $lookup: {
-          from: "colors",
-          localField: "registeredCars.carInfo.carColorId",
-          foreignField: "_id",
-          as: "carColors"
-        },
-      },
-      {
-        $lookup: {
-          from: "import_devices",
-          localField: "registeredCars.deviceInfo.imei",
-          foreignField: "imei",
-          as: "carDevices"
-        },
-      },
-      {
-        $project: {
-          "registeredCars.deviceInfo.devicePinCode": 0,
-          "carDevices.pinCode": 0
-        }
-      }
+    const codes = await db.aggregate("referral_code", []);
 
-    ]);
-
-
-
-    let userCars = [];
-    if (carsList.length > 0) {
-
-
-
-      for (let i = 0; i < carsList[0].registeredCars.length; ++i) {
-        let car = carsList[0].registeredCars[i];
-        let carBrand = {};
-        let carModel = {};
-        let carModelDetail = {};
-
-        let readyBrand = carsList[0].carBrands.find(brand => brand.CarModels.findIndex(model => model.CarModelDetails.findIndex(det => det._id.toString() == car.carInfo.carModelId.toString()) >= 0) >= 0);
-
-        if (readyBrand) {
-          readyBrand.CarModels.forEach(model => {
-            model.CarModelDetails.forEach(modelDetail => {
-              if (modelDetail._id.toString() == car.carInfo.carModelId.toString()) {
-                carBrand = { ...readyBrand };
-                carModel = { ...model };
-                carModelDetail = modelDetail;
-
-              }
-            })
-          });
-          let yy = delete carBrand['CarModels'];
-          yy = delete carModel['CarModelDetails'];
-        }
-        let color = {};
-        if (carsList[0].carColors.length > 0) {
-          color = carsList[0].carColors.find(color => color._id.toString() == car.carInfo.carColorId.toString());
-        }
-        let device = {};
-        if (carsList[0].carDevices.length > 0) {
-          device = carsList[0].carDevices.find(dev => dev.imei == car.deviceInfo.imei);
-        }
-
-
-
-        userCars.push({
-          "carId": car._id,
-          "CarId": car._id,
-          "car_id": car._id,
-          "car": car,
-          "brand": carBrand,
-          "model": carModel,
-          "modelDetail": carModelDetail,
-          "color": color,
-          "device": device
-        });
-      }
-
-
-
-    }
-
-
-
-    return responseHandler.okResponse(res, "Here you are!", userCars)
+    return responseHandler.okResponse(res, "Here you are!", codes)
 
   } catch (error) {
     console.error(error)
