a304cf5bb6c56acc92358727e18ef2a4395fcbc0
navidsavarib <navidsavarib@gmail.com>
2025-08-19 22:08:04 +0330

    Cookie

--- mobile_api/routes/auth.js
+++ mobile_api/routes/auth.js
@@ -348,7 +348,7 @@ router.post("/login", async (req, res) => {
     const maxAge = 31536000000;
     res.cookie('token', token, {
       httpOnly: true,       // Helps prevent XSS
-      secure: true,         // Use true if using HTTPS
+      secure: false,         // Use true if using HTTPS
       sameSite: 'None',   // or 'Lax' or 'None' (use 'None' if cross-site)
       maxAge: maxAge,       // 1 year in ms
       domain: "localhost"
